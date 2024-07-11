414d015cc356e4773551d591f05b5a251e9e9099
MSI <MSI@DESKTOP-4MOA5G4>
2024-07-10 19:28:26 -0500

    Cambios tales

---
+++ 01Usuarios-Samueltubay/Comunes/ConexcionBD.cs
@@ -0,0 +1,121 @@
+﻿using _01Usuarios_Samueltubay.Controllers.modelo;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace _01Usuarios_Samueltubay.Comunes
+{
+    public static class ConexionBD
+    {
+        public static SqlConnection conexion;
+
+        public static SqlConnection abrirConexion()
+        {
+            conexion = new SqlConnection("Server=DESKTOP-4MOA5G4\\\\SQLEXPRESS\r\n;Database=PROYECTO_1;Trusted_Connection=True;");
+            conexion.Open();
+            return conexion;
+        }
+
+        public static List<Usuario> GetUsuarios()
+        {
+            DataSet ds = new DataSet();
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = abrirConexion();
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+            cmd.CommandText = "SP_GET_USUARIO";
+
+            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+            adapter.Fill(ds);
+            return fillUsuarios(ds);
+
+        }
+
+        public static Usuario GetUsuario(int id)
+        {
+            DataSet ds = new DataSet();
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = abrirConexion();
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+            cmd.CommandText = "SP_GET_USUARIOU";
+            cmd.Parameters.AddWithValue("PI_ID_USUARIO", id);
+            SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+            adapter.Fill(ds);
+            return fillUsuarios(ds)[0];
+        }
+
+        public static void PostUsuario(Usuario objUsuario)
+        {
+
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = abrirConexion();
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+            cmd.CommandText = "SP_INS_USUARIO";
+            cmd.Parameters.AddWithValue("PV_CODIGO", objUsuario.codigo);
+            cmd.Parameters.AddWithValue("PI_ID_USUARIO", objUsuario.id_usuario);
+            cmd.Parameters.AddWithValue("PV_NOMBRES", objUsuario.nombres);
+            cmd.Parameters.AddWithValue("PV_APELLIDOS", objUsuario.apellidos);
+            cmd.Parameters.AddWithValue("PV_MAIL", objUsuario.mail);
+            cmd.Parameters.AddWithValue("PD_FECHA_NACIMIENTO", objUsuario.fecha_nacimiento);
+            cmd.Parameters.AddWithValue("PV_CONTRASENA", objUsuario.contrasena);
+            cmd.Parameters.AddWithValue("PI_USUARIO_CREACION", objUsuario.usuario_creacion);
+
+            cmd.ExecuteNonQuery();
+
+        }
+
+        public static void PutUsuario(int usuarioModificacion, Usuario objUsuario)
+        {
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = abrirConexion();
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+            cmd.CommandText = "SP_UPD_USUARIO";
+            cmd.Parameters.AddWithValue("PI_ID_USUARIO", objUsuario.id_usuario);
+            cmd.Parameters.AddWithValue("PV_CODIGO", objUsuario.codigo);
+            cmd.Parameters.AddWithValue("PV_NOMBRES", objUsuario.nombres);
+            cmd.Parameters.AddWithValue("PV_APELLIDOS", objUsuario.apellidos);
+            cmd.Parameters.AddWithValue("PV_MAIL", objUsuario.mail);
+            cmd.Parameters.AddWithValue("PD_FECHA_NACIMIENTO", objUsuario.fecha_nacimiento);
+            cmd.Parameters.AddWithValue("PV_CONTRASENA", objUsuario.contrasena);
+            cmd.Parameters.AddWithValue("PI_USUARIO_MODICFICACION", usuarioModificacion);
+
+            cmd.ExecuteNonQuery();
+        }
+
+        public static void DeleteUsuario(int idUsuario, int idUsuarioModificacion)
+        {
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = abrirConexion();
+            cmd.CommandType = System.Data.CommandType.StoredProcedure;
+            cmd.CommandText = "SP_DEL_USUARIO";
+            cmd.Parameters.AddWithValue("PI_ID_USUARIO", idUsuario);
+            cmd.Parameters.AddWithValue("PI_USUARIO_MODICFICACION", idUsuarioModificacion);
+
+
+            cmd.ExecuteNonQuery();
+        }
+
+        private static List<Usuario> fillUsuarios(DataSet ds)
+        {
+            List<Usuario> lrespuesta = new List<Usuario>();
+            Usuario objUsuario = new Usuario();
+            for (int i = 0; i <= ds.Tables[0].Rows.Count - 1; i++)
+            {
+                objUsuario = new Usuario();
+                objUsuario.id_usuario = Convert.ToInt32(ds.Tables[0].Rows[i]["ID_USUARIO"].ToString());
+                objUsuario.codigo = ds.Tables[0].Rows[i]["CODIGO"].ToString();
+                objUsuario.nombres = ds.Tables[0].Rows[i]["NOMBRES"].ToString();
+                objUsuario.apellidos = ds.Tables[0].Rows[i]["APELLIDOS"].ToString();
+                objUsuario.mail = ds.Tables[0].Rows[i]["MAIL"].ToString();
+                objUsuario.fecha_nacimiento = Convert.ToDateTime(ds.Tables[0].Rows[i]["FECHA_NACIMIENTO"].ToString());
+                objUsuario.contrasena = ds.Tables[0].Rows[i]["CONTRASENA"].ToString();
+                objUsuario.estado = ds.Tables[0].Rows[i]["ESTADO"].ToString();
+                objUsuario.fecha_ultima_conexion = Convert.ToDateTime(ds.Tables[0].Rows[i]["FECHA_ULTIMA_CONEXION"].ToString());
+                objUsuario.usuario_creacion = Convert.ToInt32(ds.Tables[0].Rows[i]["USUARIO_CREACION"].ToString());
+                objUsuario.fecha_creacion = Convert.ToDateTime(ds.Tables[0].Rows[i]["FECHA_CREACION"].ToString());
+                objUsuario.usuario_modificacion = Convert.ToInt32(ds.Tables[0].Rows[i]["USUARIO_MODIFICACION"].ToString());
+                objUsuario.fecha_modificacion = Convert.ToDateTime(ds.Tables[0].Rows[i]["FECHA_MODIFICACION"].ToString());
+                lrespuesta.Add(objUsuario);
+            }
+            return lrespuesta;
+        }
+    }
+}
---
+++ 01Usuarios-Samueltubay/Controllers/usuariosController.cs
@@ -0,0 +1,49 @@
+﻿using _01Usuarios_Samueltubay.Comunes;
+using _01Usuarios_Samueltubay.Controllers.modelo;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.FileSystemGlobbing.Internal.PathSegments;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace _01Usuarios_Samueltubay.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class usuariosController : ControllerBase
+    {
+        // GET: api/<usuariosController>
+        [HttpGet]
+        public List<Usuario> Get()
+        {
+            return ConexionBD.GetUsuarios();
+        }
+
+        // GET api/<usuariosController>/5
+        [HttpGet("{id}")]
+        public Usuario Get(int id)
+        {
+            return ConexionBD.GetUsuario(id);
+        }
+
+        // POST api/<usuariosController>
+        [HttpPost]
+        public void Post([FromBody] Usuario objeUsuario)
+        {
+            ConexionBD.PostUsuario(objeUsuario);
+        }
+
+        // PUT api/<usuariosController>/5
+        [HttpPut("{Usuariomodificacion}")]
+        public void Put(int Usuariomodificacion,[FromBody] Usuario objUsuario) 
+        {
+            ConexionBD.PutUsuario(Usuariomodificacion,objUsuario);
+        }
+
+        // DELETE api/<usuariosController>/5
+        [HttpDelete("{idUsuario}/{idUsuariomodificacion}")]
+        public void Delete(int idUsuario,int idUsuariomodificacion)
+        {
+            ConexionBD.DeleteUsuario(idUsuario, idUsuariomodificacion);
+        }
+    }
+}
